c30a9fd456a76972d3741900513357baf674e65a
MatheusJLV <matheusjlv@gmail.com>
2020-10-22 20:17:11 -0500

    Creation of Class Cliente

---
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/cliente.java
@@ -0,0 +1,18 @@
+package ec.edu.espol.workshops.second;
+
+public class cliente {
+	private int age;
+	private String sex;
+	private boolean maritalStatus;
+	
+	private boolean licence;
+
+	public cliente(int age, String sex, boolean maritalStatus, boolean licence) {
+		this.age = age;
+		this.sex = sex;
+		this.maritalStatus = maritalStatus;
+		this.licence = licence;
+	}
+	
+
+}
