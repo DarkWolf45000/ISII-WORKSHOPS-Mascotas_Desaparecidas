ca0d72a3699a7caf45c93390093091a4ecca2cd8
Charlesluis63 <carlos_luis63@hotmail.com>
2020-10-22 22:43:53 -0500

    Agregados set y gets

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/cliente.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/cliente.java
@@ -13,6 +13,32 @@ public class cliente {
 		this.maritalStatus = maritalStatus;
 		this.licence = licence;
 	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public void setMaritalStatus(boolean maritalStatus) {
+		this.maritalStatus = maritalStatus;
+	}
+	public void  setLicence(boolean licence) {
+		this.licence=licence;
+	}
+	public int getAge() {
+		return this.age;
+	}
 	
+	public String getSex() {
+		return this.sex;
+	}
+
+	public boolean getMaritalStatus() {
+		return this.maritalStatus;
+	}
+	public boolean getLicence() {
+		return this.licence;
+	}
+
 
 }
